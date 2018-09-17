617906937efe8e796fe9b918d5906d30a72e1855
Unknown <moomoohk@ymail.com>
2018-09-17 16:07:48 +0300

    Include package resource

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='whatsapi',
     version='',
     packages=['whatsapi', 'whatsapi.objects'],
+    package_data={'whatsapi': ['js/wapi.js']},
     url='',
     license='',
     author='moomoohk',
